ad36056f4020cd73356c130579836e093dbb5b93
Ebraheem Mahmood <ebraheemmahmood991@gmail.com>
2020-06-21 16:05:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,60 @@
 // Your code here
+function mapToNegativize(x){
+return x.map(x => x*-1)
+
+}
+
+function mapToNoChange(x){
+  return x
+}
+
+function mapToDouble(x){
+  return x.map(x => x*2)
+}
+
+function mapToSquare(x){
+  return x.map(x => Math.pow(x,2))
+}
+
+
+function reduceToTotal(x,y){
+  const reducer = (accumulator, currentValue) => accumulator + currentValue;
+  if(y === undefined){
+
+
+   return x.reduce(reducer)
+ }
+else{
+   return x.reduce(reducer,y)
+ }
+}
+
+function reduceToAllTrue(x,y){
+    x.reduce((accumulator, currentValue) => {
+      if (currentValue != undefined && y != undefined){
+         x = true
+        console.log('a')
+      }
+        else{
+     x = false
+    }
+    })
+
+       return x
+}
+
+
+function reduceToAnyTrue(x,y){
+  x.reduce((accumulator, currentValue) => {
+    if (currentValue != undefined && y != undefined){
+       x = true
+       return x
+    }
+      else{
+   x = false
+   return x
+  }
+  return x
+  })
+  return x
+}
